5af6d141b29a9c2668181adb0bd017fb7a2e0dcd
Alex Chan <a.chan@wellcome.ac.uk>
2018-06-13 17:35:40 +0100

    first pass at a bi flag

--- src/_images/2018/build_flags.py
+++ src/_images/2018/build_flags.py
@@ -38,7 +38,13 @@ PAYLOAD = {
 				'https://iiif.wellcomecollection.org/image/B0007248.jpg/0,0,3500,350/3500,350/0/default.jpg',
 				'https://iiif.wellcomecollection.org/image/B0010632.jpg/0,3250,4149,415/3500,350/0/default.jpg',
 				'https://iiif.wellcomecollection.org/image/B0008754.jpg/700,2500,3500,350/3500,350/0/default.jpg'
-		]
+		],
+        'bisexual.jpg': [
+            'https://iiif.wellcomecollection.org/image/B0008050.jpg/0,0,4414,1059/3500,840/0/default.jpg',
+            'https://iiif.wellcomecollection.org/image/B0010919.jpg/0,2500,4080,490/3500,420/0/default.jpg',
+            'https://iiif.wellcomecollection.org/image/B0010096.jpg/0,3000,4266,1023/3500,840/0/default.jpg'
+        ]
 }
 
-combine_images_vertically(images_from_urls(PAYLOAD['rainbow.jpg'])).save('rainbow.jpg')
+for name, images in PAYLOAD.items():
+    combine_images_vertically(images_from_urls(images)).save(name)
